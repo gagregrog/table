55a20cc14bfc92c29b50957330ddf50eb599ef76
Gary Gregory Rogers <gagregrog@gmail.com>
2024-03-12 21:40:11 -0700

    chore: cleanup help

--- table.sh
+++ table.sh
@@ -193,22 +193,21 @@ cat << EOF
 Everybody's favorite table flipper, lightly animated
 
 Options:
-  -f, --fps [int]                      change the running speed (default 60)
+  -f, --fps           [int]            change the running speed (default 60)
   -h, --help                           display this help
 
 Character Options:
-  -e, --eye [char(s)]                  customize flip's eyes
-  -m, --mouth [char(s)]                customize flip's mouth
-  -cl, --cheek-left [char(s)]          customize flip's left cheek
-  -cr, --cheek-right [char(s)]         customize flip's right cheek
-  -a, --arm [char(s)]                  customize flip's flippin' arms
-  -M, --motion [char(s)]               customize the table flippin' motion
+  -e,  --eye          [char(s)]        customize flip's eyes
+  -m,  --mouth        [char(s)]        customize flip's mouth
+  -cl, --cheek-left   [char(s)]        customize flip's left cheek
+  -cr, --cheek-right  [char(s)]        customize flip's right cheek
+  -a,  --arm          [char(s)]        customize flip's flippin' arms
+  -M,  --motion       [char(s)]        customize the table flippin' motion
 
 Table Options:
-  -t, --table-length [int]             change the length of the table
+  -t, --table-length  [int]            change the length of the table
 
 Scene Options:
-  [default]                            run the table flip scene
   --scan                               run the scan scene
 EOF
 }
@@ -408,25 +407,6 @@ case $SCENE in
     exit 1
     ;;
 esac
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
 #
 #
 #
